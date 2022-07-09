ce76616b8a92c9881069e5b1537c10d56380f197
Jeremiah <jaoluborode@student.lautech.edu.ng>
2022-07-09 14:28:24 +0100

    Header is completed

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header/Header.js';
+import './homepage.css'
+
+function  Homepage() {
+  return (
+    <div className='homepage'>
+     
+     <Header />
+    </div>
+  )
+}
+
+export default Homepage
